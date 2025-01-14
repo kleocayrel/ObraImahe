8937b5ac3f019d48488d1d58b3e4b05692620312
kleocayrel <kleocayrel0821@gmail.com>
2025-01-14 19:38:23 +0800

    commit

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from Project import settings
 from .views import (HomePage, AboutPage, FeedPage, FeedDetail, FeedCreate,
                     FeedUpdate, FeedDelete)
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',HomePage.as_view(), name='Home'),
     path('about/', AboutPage.as_view(), name='About'),
@@ -10,5 +13,5 @@ urlpatterns = [
     path('feed/new', FeedCreate.as_view(), name='Feed_Create'),
     path('feed/<int:pk>/edit', FeedUpdate.as_view(), name='Feed_Update'),
     path('feed/<int:pk>/delete', FeedDelete.as_view(), name='Feed_Delete'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
